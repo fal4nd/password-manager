2a86736541d8d0722c83f3d84203e960f934fd2c
fal4nd <141240206+fal4nd@users.noreply.github.com>
2023-11-29 22:43:19 +0100

    Delete project directory

--- project/target/debug/deps/main-7bf40f9adf1e40ef.d
+++
@@ -1,6 +0,0 @@
-/home/andrea/Scrivania/Password/project/target/debug/deps/main-7bf40f9adf1e40ef: src/main.rs src/insert_data.rs
-
-/home/andrea/Scrivania/Password/project/target/debug/deps/main-7bf40f9adf1e40ef.d: src/main.rs src/insert_data.rs
-
-src/main.rs:
-src/insert_data.rs:
--- project/target/debug/main.d
+++
@@ -1,1 +0,0 @@
-/home/andrea/Scrivania/Password/project/target/debug/main: /home/andrea/Scrivania/Password/project/src/insert_data.rs /home/andrea/Scrivania/Password/project/src/main.rs
